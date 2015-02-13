9accf13fefe1dc82af02f308b5ba4b8bb9831cfa
Sandeep Raghunandhan <swasandeep@yahoo.com>
2015-02-12 21:22:18 -0800

    Initial Commit

---
+++ TextSender2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextSender2
+//
+//  Created by Sandeep  Raghunandhan on 2/12/15.
+//  Copyright (c) 2015 Sandeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
